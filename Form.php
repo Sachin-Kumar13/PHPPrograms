2a3801fc6c582bc243c811bc91ac4dea366adfa8
Sachi-A <sachin.kumar1_cs17@gla.ac.in>
2020-01-19 18:30:35 +0530

    total

---
+++ Form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP FORM</title>
+</head>
+<body>
+    <form action ="ac.php" method="post">
+        Name:<input type="text" name="nm"><br>
+        Email :<input type="text" name="em"><br>
+        <input type="submit" name="submit">
+    </form>
+    
+</body>
+</html>
---
+++ ac.php
@@ -0,0 +1,6 @@
+<?php
+$name =$_POST['nm'];
+$email=$_POST['em'];
+echo "Your name is " . $name . " and your email is " . $email
+
+?>
